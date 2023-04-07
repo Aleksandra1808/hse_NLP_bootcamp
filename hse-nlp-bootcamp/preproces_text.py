8d547d5ba32d041b419fe964dc1dfafc2761b2b7
Aleksandra1808 <77661535+Aleksandra1808@users.noreply.github.com>
2023-04-07 11:28:22 +0300

    Add files via upload

---
+++ hse-nlp-bootcamp/preproces_text.py
@@ -0,0 +1,32 @@
+
+from pymorphy2 import MorphAnalyzer
+import re
+from nltk.corpus import stopwords
+from functools import lru_cache
+
+m = MorphAnalyzer()
+regex = re.compile("[А-Яа-яA-z]+")
+
+def words_only(text, regex=regex):
+    try:
+        return regex.findall(text.lower())
+    except:
+        return []
+
+@lru_cache(maxsize=128)
+def lemmatize_word(token, pymorphy=m):
+    return pymorphy.parse(token)[0].normal_form
+
+def lemmatize_text(text):
+    return [lemmatize_word(w) for w in text]
+
+
+mystopwords = stopwords.words('russian') 
+def remove_stopwords(lemmas, stopwords = mystopwords):
+    return [w for w in lemmas if not w in stopwords and len(w) > 3]
+
+def clean_text(text):
+    tokens = words_only(text)
+    lemmas = lemmatize_text(tokens)
+    
+    return ' '.join(remove_stopwords(lemmas))
